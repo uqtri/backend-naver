433abaed534083f56c24662892b6b6095193ecfc
unknown <ungquangtri@gmail.com>
2025-09-15 16:31:44 +0700

    s

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "https://jewelry-store-qk81.vercel.app",
+      "https://frontend-naverx.vercel.app/",
     ],
     credentials: true,
   })
